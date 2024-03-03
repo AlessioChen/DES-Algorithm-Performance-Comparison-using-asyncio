faed8bd4b51d5ee190d9056fbec0983c5b33ab21
Alessio Chen <alessiochen98@gmail.com>
2024-03-03 16:52:24 +0100

    feat: add tests

--- main.py
+++ main.py
@@ -11,27 +11,26 @@ from functools import partial
 from typing import List
 
 
-def read_words():
+def read_words_from_file():
     with open("words.json", 'r') as f:
         words = json.load(f)
     return words
 
 
-def get_chunks(words):
+def get_intervals(words, n_threads):
     N = len(words)
-    threads = 8
-    interval_size = N // threads
-    chunks = []
+    interval_size = N // n_threads
+    intervals = []
     start_i = 0
-    for _ in range(threads):
+    for _ in range(n_threads):
         end_i = start_i + interval_size
         if end_i > N:
             end_i = N - 1
 
-        chunks.append([start_i, end_i])
+        intervals.append([start_i, end_i])
         start_i = end_i + 1
 
-    return chunks
+    return intervals
 
 
 def sequential_solver(words):
@@ -43,8 +42,8 @@ def sequential_solver(words):
             raise Exception(f"{password} and {decrypted} are not matching")
 
 
-def parallel(words, passwords, chunk):
-    start_i, end_i = chunk[0], chunk[1]
+def parallel(words, passwords, interval):
+    start_i, end_i = interval[0], interval[1]
 
     for i in range(start_i, end_i):
         enc_to_bin = utils.str_to_bin(words[passwords[i]])
@@ -55,12 +54,12 @@ def parallel(words, passwords, chunk):
     return True
 
 
-async def parallel_solver(words, passwords):
-    chunks = get_chunks(words)
+async def parallel_solver(words, passwords, n_threads):
+    intervals = get_intervals(words, n_threads)
 
     with ProcessPoolExecutor() as process_pool:
         loop: AbstractEventLoop = asyncio.get_event_loop()
-        calls: List[partial] = [partial(parallel, words, passwords, c) for c in chunks]
+        calls: List[partial] = [partial(parallel, words, passwords, interval) for interval in intervals]
         call_coros = []
         for call in calls:
             call_coros.append(loop.run_in_executor(process_pool, call))
@@ -69,18 +68,39 @@ async def parallel_solver(words, passwords):
 
 
 async def main():
-    words = read_words()
+    times = {}
+    speedups = {}
+    n_tests = 10
+
+    words = read_words_from_file()
     passwords = list(words)
 
     start = time.time()
-    sequential_solver(words)
+    for i in range(n_tests):
+        sequential_solver(words)
     end = time.time()
-    print(f'sequential time {end - start} s')
+    seqElapsed = (end - start) / n_tests * 1000
+    print(f'sequential time {seqElapsed} ms')
 
-    start = time.time()
-    await parallel_solver(words, passwords)
-    end = time.time()
-    print(f'parallel time {end - start} s')
+    times[1] = seqElapsed
+
+    threads = [2, 4, 6, 8, 16, 32]
+    for n_threads in threads:
+        start = time.time()
+        for i in range(n_tests):
+            await parallel_solver(words, passwords, n_threads)
+
+        end = time.time()
+        parElapsed = (end - start) / n_tests * 1000
+        speedup = seqElapsed / parElapsed
+        print(f'{n_threads} threads time: {parElapsed} ms')
+        print(f'{n_threads}  threads speed up: {speedup} ms')
+
+        times[n_threads] = parElapsed
+        speedups[n_threads] = speedup
+
+    print(times)
+    print(speedups)
 
 
 if __name__ == '__main__':
--- words_gen.py
+++ words_gen.py
@@ -5,18 +5,16 @@ import des
 import json
 
 
-def generate_words(N, length):
+def generate_words(N, length=8):
     char_set = string.ascii_letters + string.digits + './'
     words = {}
     for _ in range(N):
         w = ''.join(random.choice(char_set) for _ in range(length))
         words[w] = des.encrypt(w)
 
-    return words
+    json_object = json.dumps(words, indent=4)
+    with open("words.json", "w") as f:
+        f.write(json_object)
 
 
-words = generate_words(10000, 8)
-
-json_object = json.dumps(words, indent=4)
-with open("words.json", "w") as f:
-    f.write(json_object)
+generate_words(500000, 8)
