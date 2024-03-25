ab9f6ed77e070bf76c6460c57197dd65a2231f63
Alessio Chen <alessiochen98@gmail.com>
2024-03-25 22:13:08 +0100

    refactor

--- main.py
+++ main.py
@@ -10,13 +10,6 @@ from words_gen import generate_random_passwords
 from concurrent.futures import ProcessPoolExecutor
 
 
-def read_password_from_file():
-    with open('passwords.txt', 'r') as file:
-        passwords = file.read().splitlines()
-
-    return passwords
-
-
 def get_dates() -> list[str]:
     date_range: list[str] = []
     for year in range(1900, 2025):
@@ -27,30 +20,21 @@ def get_dates() -> list[str]:
     return date_range
 
 
-def get_chunks(n_threads: int, dates: list[str]) -> list[list[int, int]]:
-    N = len(dates)
-    interval_size = N // n_threads
-    intervals = []
+def get_chunks(n_threads: int, N: int) -> list[list[int, int]]:
+    chunk_size = N // n_threads
+    chunks = []
     start_i = 0
     for _ in range(n_threads):
-        end_i = start_i + interval_size
+        end_i = start_i + chunk_size
         if end_i > N:
             end_i = N - 1
-        intervals.append(([start_i, end_i]))
+        chunks.append(([start_i, end_i]))
         start_i = end_i + 1
 
-    return intervals
+    return chunks
 
 
-def get_decrypted_passwords(encrypted_passwords: list[str]) -> list[str]:
-    res = []
-    for password in encrypted_passwords:
-        res.append(des.decrypt(utils.str_to_bin(password)))
-
-    return res
-
-
-def sequential_brute_force(passwords: str, dates: list[str]) -> None:
+def sequential_crack(passwords: list[str], dates: list[str]) -> None:
     for password in passwords:
         for date in dates:
             encrypted = des.encrypt(date)
@@ -70,26 +54,24 @@ def parallel(password: str, chunk: [[int, int]], dates: list[str]) -> None:
 
 
 async def parallel_crack(passwords: list[str], dates: list[str], n_threads: int) -> None:
-    for password in passwords:
-        await parallel_brute_force(password, dates, n_threads)
-
-
-async def parallel_brute_force(password: str, dates: list[str], n_threads: int) -> None:
-    chunks = get_chunks(n_threads, dates)
-
+    chunks = get_chunks(n_threads, len(dates))
     with ProcessPoolExecutor() as process_pool:
-        loop: AbstractEventLoop = asyncio.get_running_loop()
-        calls: List[partial] = [partial(parallel, password, chunk, dates) for chunk in chunks]
-        call_coros = []
-        for call in calls:
-            call_coros.append(loop.run_in_executor(process_pool, call))
+        for password in passwords:
+            loop: AbstractEventLoop = asyncio.get_event_loop()
+            calls: List[partial] = [
+                partial(parallel, password, chunk, dates)
+                for chunk in chunks
+            ]
+            call_coros = []
+            for call in calls:
+                call_coros.append(loop.run_in_executor(process_pool, call))
 
-        await asyncio.gather(*call_coros)
+            await asyncio.gather(*call_coros)
 
 
 async def main() -> None:
     N_TESTS = 10
-    N_TO_CRACK = 1
+    N_TO_CRACK = 5
 
     start_date = datetime.date(1900, 1, 1)
     end_date = datetime.date(2024, 12, 31)
@@ -103,7 +85,7 @@ async def main() -> None:
 
     start = time.time()
     for i in range(N_TESTS):
-        sequential_brute_force(encrypted_passwords[i], dates)
+        sequential_crack(encrypted_passwords[i], dates)
     end = time.time()
     seqElapsed = (end - start) / N_TESTS * 1000
     print(f'sequential time {seqElapsed} ms\n')
